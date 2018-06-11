a731c412eb425ff9294d64657b7c652567290ead
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-06-11 11:24:13 -0500

    Move Player Horizontally pt.2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
-    public float speed = 10f;
+	[SerializeField] float runSpeed = 5f;
 
+	private Rigidbody2D myRigibody;
 
 	// Use this for initialization
 	void Start () {
-		
+		myRigibody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Run ();
 	}
 
-    void Move()
-    {
-        
-    }
+    private void Run ()
+	{
+		float h = CrossPlatformInputManager.GetAxis ("Horizontal");
+		Vector2 playerVelocity = new Vector2 (h * runSpeed, myRigibody.velocity.y);
+		myRigibody.velocity = playerVelocity;
+	}
 }
