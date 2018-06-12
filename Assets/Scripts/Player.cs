6fff512e9d0e02ead03aafce34bd418deeb63a5a
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-06-12 10:37:13 -0500

    Animation Sate in Code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
+
+	// Config
 	[SerializeField] float runSpeed = 5f;
 
-	private Rigidbody2D myRigibody;
+	// State
+	bool isAlive = true;
 
-	// Use this for initialization
+	// Cached component references
+	Rigidbody2D myRigibody;
+	Animator myAnimator;
+
+	// Messages then methods
 	void Start () {
 		myRigibody = GetComponent<Rigidbody2D> ();
+		myAnimator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +32,10 @@ public class Player : MonoBehaviour {
 		float h = CrossPlatformInputManager.GetAxis ("Horizontal");
 		Vector2 playerVelocity = new Vector2 (h * runSpeed, myRigibody.velocity.y);
 		myRigibody.velocity = playerVelocity;
+
+		bool playerHasHorizontalSpeed = Mathf.Abs (myRigibody.velocity.x) > Mathf.Epsilon;
+		myAnimator.SetBool ("isRunning", playerHasHorizontalSpeed);
+		
 	}
 
 	private void FlipSprite () {
