8b8effbfc561ab974fb7af0efe7af938f4606c8b
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-07-02 11:38:08 -0500

    Level Layout

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadLevel (string levelName) {
+		SceneManager.LoadScene (levelName);
+	}
+
+	public void Quit () {
+		Application.Quit ();
+		print ("Quit Requested");
+	}
+}
