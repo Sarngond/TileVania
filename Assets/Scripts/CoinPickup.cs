f0b967cad0ec8f980477ba8d82e1e9c64e20d61b
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-07-20 10:27:40 -0500

    Coin Pickups

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Destroy (gameObject);
+	}
+}
