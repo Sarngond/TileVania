d95f6eba42d8fb7aa7a4bea050a9ce786bdb9230
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-07-23 11:20:16 -0500

    Persistant Score & Lives

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour {
 
 	[SerializeField] AudioClip coinPickupSFX;
+	[SerializeField] int pointsForCoinPickup = 100;
 
 	void OnTriggerEnter2D (Collider2D collider) {
 		AudioSource.PlayClipAtPoint (coinPickupSFX, Camera.main.transform.position);
+		FindObjectOfType<GameSession> ().AddToScore (pointsForCoinPickup);
 		Destroy (gameObject);
 	}
 }
