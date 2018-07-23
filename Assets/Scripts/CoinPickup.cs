9630aa34c11f5352abed2410dffebc5f4f5da0c8
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-07-23 10:56:18 -0500

    Persistant SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour {
 
+	[SerializeField] AudioClip coinPickupSFX;
+
 	void OnTriggerEnter2D (Collider2D collider) {
+		AudioSource.PlayClipAtPoint (coinPickupSFX, Camera.main.transform.position);
 		Destroy (gameObject);
 	}
 }
