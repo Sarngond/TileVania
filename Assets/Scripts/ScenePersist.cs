aee2a831ea1251ee7fa6f0be6d5f3c499b776e91
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-07-23 11:39:25 -0500

    Remembering Pickups

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+
+    [SerializeField] int startingBuildIndex;
+
+    void Awake()
+    {
+        int numScenePersists = FindObjectsOfType<ScenePersist>().Length;
+        if (numScenePersists > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        startingBuildIndex = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex != startingBuildIndex)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
